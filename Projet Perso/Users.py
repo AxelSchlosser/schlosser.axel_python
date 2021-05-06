d352d260a7971063c39cb0210d3973ff89c87571
Axel Schlosser <axel.schlosser@gmail.com>
2021-05-06 09:53:45 +0200

    ajout des films au json

--- Projet Perso/Users.py
+++ Projet Perso/Users.py
@@ -14,13 +14,6 @@ class Users():
                 pass
             else:
                 i = 1
-                while i < 6:
-                    #resp = json.loads(requests.get("https://www.netflix.com/api/shakti/v235006dc/viewingactivity?pg=" + str(i) + "&pgSize=20&guid=" + userID).content)
-                    url = "https://www.netflix.com/api/shakti/v235006dc/viewingactivity?pg=" + str(i) + "&pgSize=20&guid=" + userID + "&_=1620226036865&authURL=1620205575102.zIvYfarf%2F0OaEesF85FD%2BIFFgsc%3D"
-                    resp = requests.get(url)
-                    print(url)
-                    print(resp)
-                    i+=1
     
     
     def getUserName(self):
